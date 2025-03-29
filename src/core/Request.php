4269091e18997d6c090e30e5f487ba2d465ffbc4
Manuel <caballeromanuel203@gmail.com>
2025-03-30 04:13:47 +0800

    added request query support

--- src/core/Request.php
+++ src/core/Request.php
@@ -8,12 +8,13 @@ use AllowDynamicProperties;
 class Request
 {
     public string $uri, $method;
-    public $param;
+    public Param $param;
 
     function __construct()
     {
         $this->loadFormData();
-        $this->uri = $_SERVER['REQUEST_URI'];
+        $uri = str_replace("/index.php", "", $_SERVER['PHP_SELF']);
+        $this->uri = empty($uri) ? "/" : $uri;
         $this->method = $_SERVER['REQUEST_METHOD'];
         $this->param = new Param();
     }
